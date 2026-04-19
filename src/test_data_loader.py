8f59be81fcd09d33f119db4412dda07d95b993f6
E.C.T.D. Bouwman <s2832674@vibranium.liacs.nl>
2026-04-19 14:02:51 +0200

    add pytorch dataset class for dataloader

--- src/test_data_loader.py
+++ src/test_data_loader.py
@@ -1,20 +1,39 @@
 import os
 import duckdb
+from PIL import Image
 from pathlib import Path
 import pandas as pd
 from src.data_gather import DuckDBManager
+from torch.utils.data import Dataset, DataLoader
 from dotenv import load_dotenv
+
+
 load_dotenv(".env")
+
 
 base_folder  = Path(os.getenv("DATA_FOLDER"))
 db_path = base_folder / os.getenv("DATABASE")
 
+class CrypticBioDataset(Dataset):
+    def __init__(self, db_path):
+        self.manager = DuckDBManager(db_path, read_only=True)
 
-with DuckDBManager(db_path, read_only=True) as db:
-    for row in db.con.execute("""
-        SELECT id, crypticbio_image, sentinel_image
-        FROM crypticbio
-        LIMIT 100
-    """).fetchall():
-        
+        with self.manager as db:
+            self.data = db.con.execute("""
+                SELECT id, crypticbio_image, sentinel_image
+                FROM crypticbio
+                ORDER BY RANDOM()
+            """).fetchall()
+    
+    def __len__(self):
+        return len(self.data)
 
+    def __getitem__(self, idx):
+        _id, cb_path, sh_path = self.data[idx]
+
+        return {
+            "id": _id,
+            "cb": cb_path,
+            "sh": sh_path
+        }
+      
