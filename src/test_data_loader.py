1a2d5ac96d5a6cc0d1f774d89f082a95b069f906
E.C.T.D. Bouwman <s2832674@vibranium.liacs.nl>
2026-04-20 12:49:15 +0200

    add _load_image function

--- src/test_data_loader.py
+++ src/test_data_loader.py
@@ -16,7 +16,7 @@ db_path = base_folder / os.getenv("DATABASE")
 
 class CrypticBioDataset(Dataset):
     def __init__(self, db_path):
-        self.manager = DuckDBManager(db_path, read_only=True)
+        self.manager = DuckDBManager(db_path)
 
         with self.manager as db:
             self.data = db.con.execute("""
@@ -27,14 +27,22 @@ class CrypticBioDataset(Dataset):
     
     def __len__(self):
         return len(self.data)
+    
+    def _load_image(self, path):
+        img = Image.open(path).convert("RGB")
+        arr = np.array(img)
+        return torch.tensor(arr).permute(2, 0, 1).float() / 255.0
 
     def __getitem__(self, idx):
         _id, cb_path, sh_path = self.data[idx]
 
+        cb_img = self._load_image(cb_path)
+        sh_img = self._load_image(sh_path)
+
         return {
             "id": _id,
-            "cb": cb_path,
-            "sh": sh_path
+            "cb": cb_img,
+            "sh": sh_img
         }
     
       
