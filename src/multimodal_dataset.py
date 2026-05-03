367f583808a8b5d8241d4b9db43a32ec289e538a
E.C.T.D. Bouwman <s2832674@tritanium.liacs.nl>
2026-05-03 09:11:45 +0200

    changed dataset

--- src/multimodal_dataset.py
+++ src/multimodal_dataset.py
@@ -1,77 +1,30 @@
-import os
 import torch
 from PIL import Image
-from pathlib import Path
 from src.data_gather import DuckDBManager
 from torch.utils.data import Dataset
-from dotenv import load_dotenv
-
-
-class TransformDataset(Dataset):
-    def __init__(self, subset, transform=None):
-        self.subset = subset
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.subset)
-
-    def __getitem__(self, index):
-        cb, sh, label = self.subset[index]
-        if self.transform:
-            cb = self.transform(cb)
-            sh = self.transform(sh)
-        return cb, sh, label
 
 
 class CrypticBioDataset(Dataset):
-    def __init__(self, ids, name_to_id, cb_folder, sh_folder, db_path, limit=None):
-        self.ids = ids[:limit] if limit else ids
+    def __init__(self, ids, name_to_id, db_path):
         self.name_to_id = name_to_id
-        self.cb_folder = Path(cb_folder)
-        self.sh_folder = Path(sh_folder)
+        
 
         with DuckDBManager(db_path) as db:
-            df = db.con.execute("""
-                SELECT id, scientificName
-                FROM crypticbio
-            """).df()
-
-        self.id_to_label = dict(zip(df["id"].astype(str), df["scientificName"]))
-
-        self.sh_lookup = {}
-
-        for f in self.sh_folder.glob("*.png"):
-            file_id = f.name.split("_")[0]
-            self.sh_lookup[file_id] = f
+            self.data = db.con.execute("""
+                SELECT id, crypticbio_image, sentinel_image, scientificName
+                FROM crypticbio WHERE id = ANY(?)""", 
+                [ids]).df()
 
 
     def __len__(self):
-        return len(self.ids)
-    
-
-    def _load_image(self, path, size=(224, 224)):
-        return Image.open(path).convert("RGB").resize(size, Image.BILINEAR)
+        return len(self.data)
     
 
     def __getitem__(self, idx):
-        _id = str(self.ids[idx])
-
-        cb_path = self.cb_folder / f"{_id}.png"
-        if not cb_path.exists():
-            raise FileNotFoundError(f"Missing CB image: {cb_path}")
-
-        sh_path = self.sh_lookup.get(_id)
-        if sh_path is None:
-            raise FileNotFoundError(f"Missing SH image for id {_id}")
-
-        cb_img = self._load_image(cb_path)
-        sh_img = self._load_image(sh_path)
-
-        label_name = self.id_to_label.get(_id)
-        if label_name is None:
-            raise ValueError(f"no label for id {_id}")
-        label = self.name_to_id[label_name]
-
-        return cb_img, sh_img, torch.tensor(label)
+        row = self.data.iloc[idx]
+        cb_img = Image.open(row['crypticbio_image']).convert('RGB')
+        sh_img = Image.open(row['sentinel_image']).convert('RGB')
+        label  = self.name_to_id[row['scientificName']]
+        return cb_img, sh_img, torch.tensor(label, dtype=torch.long)
     
 
--- src/train.py
+++ src/train.py
@@ -4,11 +4,11 @@ import torch.nn as nn
 from tqdm import tqdm
 from pathlib import Path
 from early_fusion import EarlyFusionModel
-from model_transformer import get_transforms
+from model_transformer import get_transforms, Transform
 from src.data_gather import DuckDBManager
-from multimodal_dataset import CrypticBioDataset, TransformDataset
+from multimodal_dataset import CrypticBioDataset
 from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from dotenv import load_dotenv
 
 
@@ -85,20 +85,19 @@ def main():
     dataset = CrypticBioDataset(
         ids=valid_ids,
         name_to_id=name_to_id,
-        cb_folder=cb_folder,
-        sh_folder=sh_folder,
-        db_path=db_path
+        db_path=db_path,
     )
 
-    labels_array = [dataset.id_to_label[str(i)] for i in valid_ids]
+    id_to_label = dict(zip(filtered["id"].astype(str), filtered["scientificName"]))
+    labels_array = [id_to_label[str(i)] for i in valid_ids]
     train_idx, temp_idx = train_test_split(range(len(dataset)), test_size=0.2, stratify=labels_array, random_state=42)
     val_idx, test_idx = train_test_split(temp_idx, test_size=0.5, stratify=[labels_array[i] for i in temp_idx], random_state=42)
 
-    data_transforms = get_transforms(transform_size=-1, normalize=([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]))
+    data_transforms = get_transforms(transform_size=-1, normalize=([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]))
 
-    train_ds = TransformDataset(torch.utils.data.Subset(dataset, train_idx), transform=data_transforms['train'])
-    val_ds   = TransformDataset(torch.utils.data.Subset(dataset, val_idx),   transform=data_transforms['val'])
-    test_ds  = TransformDataset(torch.utils.data.Subset(dataset, test_idx),  transform=data_transforms['test'])
+    train_ds = Transform(Subset(dataset, train_idx), transform=data_transforms['train'])
+    val_ds   = Transform(Subset(dataset, val_idx),   transform=data_transforms['val'])
+    test_ds  = Transform(Subset(dataset, test_idx),  transform=data_transforms['test'])
 
     print(f"Train: {len(train_ds)} | Val: {len(val_ds)} | Test: {len(test_ds)}") 
 
@@ -107,35 +106,40 @@ def main():
 
     model = EarlyFusionModel(num_classes=len(name_to_id), freeze_backbone=True).to(device)
     criterion = nn.CrossEntropyLoss()
+    # optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=0.01)
     optimizer = torch.optim.AdamW([
         {'params': model.channel_proj.parameters(), 'lr': 1e-4},
         {'params': model.classifier.parameters(),   'lr': 1e-4},
     ], weight_decay=0.01)
 
-    train_loader = DataLoader(train_ds, batch_size=16, shuffle=True,  num_workers=4, pin_memory=True)
-    val_loader   = DataLoader(val_ds,   batch_size=16, shuffle=False, num_workers=4, pin_memory=True)
+    train_loader = DataLoader(train_ds, batch_size=16, shuffle=True,  num_workers=8, pin_memory=True)
+    val_loader   = DataLoader(val_ds,   batch_size=16, shuffle=False, num_workers=8, pin_memory=True)
 
     
     best_val_acc = 0.0
     best_val_loss = float('inf')
     epochs_no_improve = 0
-    patience = 5
+    patience = 10
     early_stop = False
+    num_epochs = 100
 
-    for epoch in range(20): 
+    for epoch in range(num_epochs): 
         if early_stop:
             print("Early stopping.")
             break
 
-        print(f"\nEpoch {epoch+1}/20")
+        print(f"\nEpoch {epoch+1}/{num_epochs}")
 
         if epoch == 5:
             print("Unfreezing backbone...")
+            epochs_no_improve = 0  
+            best_val_loss = float('inf')  
+
             for param in model.backbone.parameters():
                 param.requires_grad = True
 
             optimizer = torch.optim.AdamW([
-                {'params': model.backbone.parameters(),     'lr': 1e-6},
+                {'params': model.backbone.parameters(),     'lr': 1e-5},
                 {'params': model.channel_proj.parameters(), 'lr': 1e-4},
                 {'params': model.classifier.parameters(),   'lr': 1e-4},
             ], weight_decay=0.01)
@@ -164,6 +168,17 @@ def main():
 
         if epochs_no_improve >= patience:
             early_stop = True
+    
+    try:
+        checkpoint = torch.load("best_early_fusion.pth", map_location=device)
+        model.load_state_dict(checkpoint['model_state_dict'])
+    except FileNotFoundError:
+        print("No checkpoint found")
+        return
+
+    test_loader = DataLoader(test_ds, batch_size=16, shuffle=False, num_workers=8, pin_memory=True)
+    _, test_acc = evaluate(model, test_loader, criterion, device)
+    print(f"\nFinal Test Accuracy: {test_acc:.2f}%")
 
 if __name__ == "__main__":
     main()
