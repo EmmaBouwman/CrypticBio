fc2e592e3d0d3122154fe3318af6e83f8f5ea55d
Aart Rosmolen <s2548526@vuw.leidenuniv.nl>
2026-05-05 13:08:36 +0200

    toevoegen en testen van gradcam

---
+++ slurm/gradcam.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=gradcam
+#SBATCH --output=logs/job_%x_%j.out
+#SBATCH --mail-user="s2548526@vuw.leidenuniv.nl"
+#SBATCH --mail-type="END"
+#SBATCH --mem=64G
+#SBATCH --time=00:30:00
+#SBATCH --partition=gpu-short
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+
+mkdir -p logs
+
+module purge
+
+module load ALICE/default
+module load slurm
+module load CUDA/12.3.2
+module load uv
+
+nvidia-smi
+
+# Syncing the uv environment
+echo "Starting to sync uv"
+uv sync
+
+echo "Starting job for vit_tiny_patch16_224"
+
+# Run the script with your requested parameters
+uv run scripts/gradcam.py \
+  --model_path best_animal_base_resized.pth \
+  --model_type 1 \
+  --model_name vit_base_patch16_224 \
+  --output_dir results_analysis
