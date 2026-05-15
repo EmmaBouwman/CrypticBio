bdfc04375f9680913b5cb44281e0acc394aaad4a
Emma Bouwman <s2832674@nodelogin03.alice.leidenuniv.nl>
2026-05-15 13:00:41 +0200

    changed create plots slurm file for new plots

--- slurm/create_plots.sh
+++ slurm/create_plots.sh
@@ -4,19 +4,24 @@
 #SBATCH --mail-user="s2832674@vuw.leidenuniv.nl"
 #SBATCH --mail-type="END"
 #SBATCH --mem=4G
-#SBATCH --time=0:05:00
+#SBATCH --time=0:10:00
 #SBATCH --partition=cpu-short
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 
-set -e
-
 module purge
 module load ALICE/default
 
+mkdir -p plots
+
+source .venv/bin/activate
 
 echo "Starting plotting job (Job ID: $SLURM_JOB_ID, host: $(hostname))"
-uv sync
-uv run scripts/plot_from_out.py logs/*.out
+
+python scripts/plot_from_out.py logs/early_fusion_final/*.out
+python scripts/plot_from_out.py logs/late_fusion_final/*.out
+python scripts/plot_from_out.py logs/gated_fusion_final/*.out
+python scripts/plot_from_out.py logs/transformer_final/*.out
+
 echo "Done!"
